cdb2cb3480ebf323431ae5a779f180942df1ea07
Ryan06080410 <ryandepaulaifb@gmail.com>
2026-09-03 13:41:31 +0000

    aula 03/09

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, session
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "aí sim"
 
 @app.route("/")
 def login():
@@ -8,15 +9,33 @@ def login():
 
 @app.route("/painel")
 def painel():
-    return render_template("painel.html")
+    if 'usuario_nome' in session: 
+        return render_template("painel.html",usuario_nome=session['usuario_nome'],usuario_cpf=session['usuario_cpf'])
+    return redirect('/')
 
 @app.route("/verificar", methods=['POST'])
 def verificar():
     cpf = request.form.get('cpf')
     senha = request.form.get('senha')
     print('Tentando login com: ', cpf,'/senha: ',senha)
+    
+    if cpf == "04102009" and senha == "0410":
+        session['usuario_nome'] = "Yanni"
+        session['usuario_cpf'] = cpf
+        return redirect("/painel")
+
+    if cpf == "24052024" and senha == "1234":
+        session['usuario_nome'] = "Leonardo"
+        session['usuario_cpf'] = cpf
+        return redirect("/painel")
+
     return redirect('/')
 
+@app.route('/sair')
+def sair():
+    session.pop('usuario_nome',None)
+    session.pop('usuario_cpf',None)
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run(debug=True)
